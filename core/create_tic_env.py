473f0296268e5e9cf3622a3e368ab38a17f81863
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-05-05 19:36:13 -0400

    Added resolve_path() to easily create paths from given args
     - Previous pilot code removed.

--- core/create_tic_env.py
+++ core/create_tic_env.py
@@ -1,6 +1,17 @@
 #!/python3
 import os
 
-cwd = os.getcwd()
-root_name = 'tic_env'
-print(cwd)
+
+def resolve_path(some_path = '', *dirs):
+    if len(dirs) == 0:
+        return os.path.realpath(os.path.expanduser(some_path))
+    # if os.sep in some_path:
+    some_path_parts = some_path.split(os.sep)
+    some_path_parts.extend(dirs)
+    joined_dirs = os.sep.join(some_path_parts)
+    return os.path.realpath(os.path.expanduser(joined_dirs))
+
+
+
+
+
