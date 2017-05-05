faeeac87f99a1f3934b6ac4020a44f0533e02a37
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-05-05 19:41:49 -0400

    Added paths_setup() to create abs_paths for initial setup
     - returns a dict with path for direnv binary

--- core/create_tic_env.py
+++ core/create_tic_env.py
@@ -11,7 +11,16 @@ def resolve_path(some_path = '', *dirs):
     joined_dirs = os.sep.join(some_path_parts)
     return os.path.realpath(os.path.expanduser(joined_dirs))
 
-
-
+def paths_setup(root_name = 'tic_env'):
+    script_path = resolve_path(__file__)
+    path_dirs = script_path.split(os.sep)
+    root_idx = max({idx for idx, elem in enumerate(path_dirs) if elem == root_name and path_dirs[idx+1] == 'core'}) + 1
+    root_dirs = path_dirs[0:root_idx]
+    root_path = os.sep.join(root_dirs)
+    rel_dirs = {'direnv': ['requirements', 'bin', 'direnv.linux-amd64']}
+    temp_list = root_dirs
+    temp_list.extend(rel_dirs['direnv'])
+    abs_paths = {'direnv': os.sep.join(temp_list)}
+    return abs_paths
 
 
