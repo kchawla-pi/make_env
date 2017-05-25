ef6e90edb6ab92f5b77a89bbcec8632782d85f3a
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-05-25 16:02:53 -0400

    Added: test_copy_binary, exception handling. Refactored: MyTestCase => 3 classes
      - MyTestCase class was refactored into 3 classes to ascertain if that \
      would add predictability to order of testing. (It didn't)
      - Removed teardown() as it added to more confusion than help
      - Changed some utilities to enable optional params for debug messages \
      & error handling

---
+++ tests/test_SubShell.py
@@ -0,0 +1,146 @@
+def add_path():
+    root = __file__.split(os.sep)[0:-2]
+    path = os.path.join(*[*root, 'src'])
+    os.sys.path.append(path) if path not in os.sys.path \
+        else print("{} already in sys.path".format(path))
+        
+
+import unittest
+import os
+import collections
+try:
+    import toolkit
+except ImportError:
+    add_path()
+    import toolkit
+try:
+    import subshell
+except ImportError:
+    add_path()
+    import subshell
+
+
+class TestSetupPaths(unittest.TestCase):
+    """
+    Tests for Class SubShell
+    """
+
+    def setUp(self):
+        self.subshell = subshell.SubShell(purpose='test')
+        self.suffix = self.subshell.suffix
+        self.subshell.setup_paths()
+        tester_file_path = os.path.realpath(__file__)
+        testee_module_path = os.path.realpath(subshell.__file__)
+        self.project_root = os.path.commonpath([tester_file_path, testee_module_path])
+        self.shell_names= ('bash', 'zsh', 'fish', 'tcsh')
+        Paths = collections.namedtuple('Paths', 'setupfile installedfile installationpath backupspath')
+        installationpath = os.path.expanduser(os.path.join('~', 'bin'+self.suffix, 'direnv'))
+        self.paths = Paths(
+            setupfile=os.path.join(self.project_root, 'requirements', 'bin', 'direnv.linux-amd64'),
+            installationpath=installationpath,
+            installedfile=os.path.join(installationpath, 'direnv.linux-amd64'),
+            backupspath=os.path.join(installationpath, 'pre_direnv_backups')
+            )
+
+    def test_setup_paths(self):
+        print('test_setup_paths')
+        expected = self.paths
+        observed = self.subshell.setup_paths()
+        
+        try:
+            self.assertEqual(expected, observed)
+        except AssertionError:
+            print('=' * 10)
+            for e,o in zip(expected, observed):
+                print('e:\t', e, '\n', 'o:\t', o, '\n', sep='')
+            print('=' * 10)
+            raise AssertionError
+
+
+class TestMakeDirs(unittest.TestCase):
+    """
+    Tests for Class SubShell
+    """
+    
+    def setUp(self):
+        self.subshell = subshell.SubShell(purpose='test')
+        self.suffix = self.subshell.suffix
+        self.subshell.setup_paths()
+        tester_file_path = os.path.realpath(__file__)
+        testee_module_path = os.path.realpath(subshell.__file__)
+        self.project_root = os.path.commonpath([tester_file_path, testee_module_path])
+        self.shell_names = ('bash', 'zsh', 'fish', 'tcsh')
+        Paths = collections.namedtuple('Paths', 'setupfile installedfile installationpath backupspath')
+        installationpath = os.path.expanduser(os.path.join('~', 'bin' + self.suffix, 'direnv'))
+        self.paths = Paths(
+            setupfile=os.path.join(self.project_root, 'requirements', 'bin', 'direnv.linux-amd64'),
+            installationpath=installationpath,
+            installedfile=os.path.join(installationpath, 'direnv.linux-amd64'),
+            backupspath=os.path.join(installationpath, 'pre_direnv_backups')
+            )
+        
+    def test_make_dirs(self):
+        print('test_make_dirs')
+        # try:
+        #     self.assertFalse(os.path.exists(self.subshell.paths.installationpath))
+        # except AssertionError:
+        #     toolkit.cleanup_tree(os.path.split(self.subshell.paths.installationpath)[0], handle_exceptions=False)
+        # finally:
+        #     self.assertFalse(os.path.exists(self.subshell.paths.installationpath))
+        toolkit.change_permissions(os.path.split(self.paths.installationpath)[0], handle_exceptions=False, notify_init=True)
+        toolkit.cleanup_tree(os.path.split(self.paths.installationpath)[0], handle_exceptions=False, notify_init=True, notify_outcome=True)
+        self.subshell.make_dirs()
+        self.assertTrue(os.path.exists(self.paths.installationpath))
+        self.assertTrue(os.path.exists(self.paths.backupspath))
+        
+        # print(os.path.exists(os.path.exists(self.subshell.paths.backupspath)),\
+        #       self.subshell.paths.backupspath)
+
+
+class TestCopyBinary(unittest.TestCase):
+    """
+    Tests for Class SubShell
+    """
+    
+    def setUp(self):
+        self.subshell = subshell.SubShell(purpose='test')
+        self.suffix = self.subshell.suffix
+        self.subshell.setup_paths()
+        self.subshell.make_dirs()
+        tester_file_path = os.path.realpath(__file__)
+        testee_module_path = os.path.realpath(subshell.__file__)
+        self.project_root = os.path.commonpath([tester_file_path, testee_module_path])
+        self.shell_names = ('bash', 'zsh', 'fish', 'tcsh')
+        Paths = collections.namedtuple('Paths', 'setupfile installedfile installationpath backupspath')
+        installationpath = os.path.expanduser(os.path.join('~', 'bin' + self.suffix, 'direnv'))
+        self.paths = Paths(
+            setupfile=os.path.join(self.project_root, 'requirements', 'bin', 'direnv.linux-amd64'),
+            installationpath=installationpath,
+            installedfile=os.path.join(installationpath, 'direnv.linux-amd64'),
+            backupspath=os.path.join(installationpath, 'pre_direnv_backups')
+            )
+    
+    def test_copy_binary(self):
+        print('test_copy_binary')
+        try:
+            self.assertFalse(os.path.exists(self.subshell.paths.installedfile))
+        except Exception as excep:
+            toolkit.change_permissions(self.subshell.paths.installedfile)
+            os.remove(self.subshell.paths.installedfile)
+            # raise excep
+        exceptions_list = self.subshell.copy_binary(max_attempts=2)
+        try:
+            self.assertTrue(os.path.exists(self.subshell.paths.installedfile))
+        except AssertionError:
+            print(exceptions_list)
+            raise AssertionError
+        
+        pass
+        # toolkit.cleanup_tree(os.path.split(self.subshell.paths.installationpath)[0])
+
+
+if __name__=='__main__':
+    # unittest.main()
+    test_make_dirs = TestMakeDirs
+    test_make_dirs.test_make_dirs()
+    pass
