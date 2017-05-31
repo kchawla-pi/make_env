5964ca3f87bcdb96ffc343d6c9a237566c20ba02
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-05-31 19:18:59 -0400

    Added tests for subshell.SubShell methods, unittest framework scrapped
     - Imported and used toolkit.py functions
     - setup_tests()
     - test_setup_tests()
     - test_make_dirs()
     - test_copy_binary()
     - test_make_exec()

--- tests/test_subshell_2.py
+++ tests/test_subshell_2.py
@@ -1,6 +1,6 @@
 def add_path():
     root = __file__.split(os.sep)[0:-2]
-    path = os.path.join(*[*root, 'src'])
+    path = os.path.realpath(os.path.join(*[*root, 'src']))
     os.sys.path.append(path) if path not in os.sys.path \
         else print("{} already in sys.path".format(path))
 
@@ -8,7 +8,9 @@ def add_path():
 # import unittest
 import os
 import collections
+import shutil
 from pprint import pprint
+import subprocess
 
 
 try:
@@ -23,33 +25,69 @@ except ImportError:
     import subshell
 
 
-testee = subshell.SubShell(purpose='test')
+# Establishing necessary paths to run the tests.
+def setup_tests():
+    tester_file_path = os.path.realpath(__file__)
+    testee_module_path = os.path.realpath(subshell.__file__)
+    project_root = os.path.commonpath([tester_file_path, testee_module_path])
+    
+    # setting up path for benchmarks .
+    suffix = '_test'
+    installationpath = os.path.expanduser(os.path.join('~', 'bin' + suffix, 'direnv'))
+    Paths = collections.namedtuple('Paths', 'setupfile installedfile installationpath backupspath')
+    benchmark_paths = Paths(
+                setupfile=os.path.join(project_root, 'requirements', 'bin', 'direnv.linux-amd64'),
+                installationpath=installationpath,
+                installedfile=os.path.join(installationpath, 'direnv.linux-amd64'),
+                backupspath=os.path.join(installationpath, 'pre_direnv_backups')
+                )
+    assert([False, True, True, True] == ['test' in path for path in benchmark_paths])
+    return benchmark_paths
 
-tester_file_path = os.path.realpath(__file__)
-testee_module_path = os.path.realpath(subshell.__file__)
-project_root = os.path.commonpath([tester_file_path, testee_module_path])
-
-suffix = '_test'
-installationpath = os.path.expanduser(os.path.join('~', 'bin' + suffix, 'direnv'))
-Paths = collections.namedtuple('Paths', 'setupfile installedfile installationpath backupspath')
-tester_file_path = Paths(
-            setupfile=os.path.join(project_root, 'requirements', 'bin'+suffix, 'direnv.linux-amd64'),
-            installationpath=installationpath,
-            installedfile=os.path.join(installationpath, 'direnv.linux-amd64'),
-            backupspath=os.path.join(installationpath, 'pre_direnv_backups')
-            )
-
-
-def test_setup_paths(testee_paths):
-    testee_paths = testee.setup_paths()
-    return testee_paths
+    
+def test_setup_paths(testee, benchmark_paths):
+    assert(benchmark_paths == testee.setup_paths())
+    return 'Passed.'
     
     
+def test_make_dirs(testee):
+    toolkit.check_remove(testee.paths.installationpath)
+    testee.make_dirs()
+    assert(os.path.exists(testee.paths.backupspath) is True)
+    return 'Passed.'
+
     
+def test_copy_binary(testee):
+    toolkit.check_remove(testee.paths.installedfile)
+    toolkit.check_make(testee.paths.installationpath)
+    testee.copy_binary(max_attempts=3)
+    assert(os.path.isfile(testee.paths.installedfile) is True)
+    return 'Passed.'
+    
+    
+def test_make_exec(testee):
+    if os.name == 'nt':
+        return
+    testee.copy_binary()
+    os.chmod(testee.paths.installedfile, 0o666)
+    pre_fn_permit = toolkit.get_file_permission_via_shell(testee.paths.installedfile, in_form='code')
+    assert(pre_fn_permit == '0o666')
+    testee.make_exec()
+    post_fn_permit = toolkit.get_file_permission_via_shell(testee.paths.installedfile, in_form='code')
+    assert(post_fn_permit == '0o111')
+    return 'Passed.'
+    
+   
 if __name__ == '__main__':
+    incr = toolkit.increment(start=1, step=1)
+    testee = subshell.SubShell(purpose='test')  # Instance of SubShell, which will be tested
+    benchmark_paths = setup_tests()
+    print(next(incr), test_setup_paths(testee, benchmark_paths))
+    print(next(incr), test_make_dirs(testee))
+    print(next(incr), test_copy_binary(testee))
+    print(next(incr), test_make_exec(testee))
     
-    pprint(testee.__dict__)
-    testee_paths = testee.setup_paths()
+    
     
 
 
