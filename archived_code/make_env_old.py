68d1b6a7186f30813c503c28b9fe34b673a75878
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2017-06-02 05:27:37 -0400

    Refactored unused functions from./src/make_env/py into ./archived_code

--- archived_code/make_env_old.py
+++ archived_code/make_env_old.py
@@ -42,5 +42,55 @@ def identify_shell(param_shell='bash', force='no'):  # default parameter added h
              'file': shell_config_file}
     return shell
 
+def install_direnv(shell, direnv_paths, max_attempts=3):
+    os.makedirs(direnv_paths['backupspath'])
+    copy_direnv(shell, direnv_paths, max_attempts)
+    make_exec(shell, direnv_paths)
+    with open(shell['file'], 'a') as write_obj:
+        write_obj.writelines(shell['command'])
+    # os.environ["PATH"] += os.pathsep + direnv_paths['installpath']
+
+
+def uninstall_direnv(shell):
+    with open(shell['file'], 'r') as read_obj:
+        contents = read_obj.readlines()
+    remove_line_idx = {idx for idx, line_ in enumerate(contents) if shell['command'] in line_}
+    new_contents = [line_ for idx, line_ in enumerate(contents) if idx not in remove_line_idx]
+    with open(shell['file'], 'w') as write_obj:
+        write_obj.write('\n'.join(new_contents))
+
+
+def new_subshell(target_dir, subshell_name):
+    direnv_config_init = "export subshellname=".join(subshell_name)
+    target_path = os.sep.join(target_dir, ".envrc")
+    with open(target_path, 'w') as write_obj:
+        write_obj.writelines(direnv_config_init)
+
+
+def backup(shell, direnv_paths):
+    backup_shell_config(shell, direnv_paths, msg=True)
+    backup_path_var(direnv_paths, msg=True)
+
+
+def direnv_handler(task='check'):
+    direnv_paths = setup_paths()
+    shell = identify_shell()
+    installed = check_direnv(shell)
+
+    if task == 'install':
+        if installed:  # uninstall existing installation first
+            uninstall_direnv(shell, direnv_paths)
+        backup(shell, direnv_paths)
+        install_direnv(shell, direnv_paths)
+        installed = check_direnv(shell)
+    elif task == 'uninstall' and installed:
+        uninstall_direnv(shell, direnv_paths)
+        installed = check_direnv(shell)
+    print("'direnv' is installed at", direnv_paths['installpath']) if installed else print("'direnv' is not installed.")
+
+
+if __name__ == '__main__':
+    direnv_handler()
+    # new_subshell(target_dir, subshell_name)
 
 #TODO: direnv is a good candidate for a Class. maybe backup+restore in one too. Shell?
